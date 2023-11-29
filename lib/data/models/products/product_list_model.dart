6cbc805fe46965222c3cebee530d719dc4278d66
Aakansha Thakur <98897384+AakanshaThakur@users.noreply.github.com>
2023-11-29 17:24:02 +0530

    Sassify_Adding_GST_Parameter: Implemented the changes

--- lib/data/models/products/product_list_model.dart
+++ lib/data/models/products/product_list_model.dart
@@ -34,33 +34,33 @@ class FetchProductListModel {
 
 class ProductWithVariant {
   final int categoryId;
-  final dynamic currency;
-
   final String categoryName;
   final bool categoryActive;
   final int productId;
   final String productName;
-  final String? brandName;
-  final int? brandId;
+  final String brandName;
+  final int brandId;
   final int variantId;
-  final double? cost;
-  final double? quantity;
-  final double? discountPercent;
-  final int? stock;
-  final String? productDescription;
+  final int cost;
+  final int quantity;
+  final int discountPercent;
+  final int stock;
+  final int stockId;
+  final String productDescription;
   final List<String> images;
   final String unit;
+  final bool variantActive;
   final int barcode;
   final bool draft;
-  final bool variantActive;
-  final int? restockReminder;
+  final int restockReminder;
+  final int sgst;
+  final int cgst;
+  final String currency;
 
   ProductWithVariant({
-    required this.currency,
-    required this.categoryActive,
-    required this.variantActive,
     required this.categoryId,
     required this.categoryName,
+    required this.categoryActive,
     required this.productId,
     required this.productName,
     required this.brandName,
@@ -70,18 +70,22 @@ class ProductWithVariant {
     required this.quantity,
     required this.discountPercent,
     required this.stock,
+    required this.stockId,
     required this.productDescription,
     required this.images,
     required this.unit,
+    required this.variantActive,
     required this.barcode,
     required this.draft,
     required this.restockReminder,
+    required this.sgst,
+    required this.cgst,
+    required this.currency,
   });
 
   factory ProductWithVariant.fromJson(Map<String, dynamic> json) =>
       ProductWithVariant(
         categoryId: json["category_id"],
-        currency: json["currency"],
         categoryName: json["category_name"],
         categoryActive: json["category_active"],
         productId: json["product_id"],
@@ -93,13 +97,17 @@ class ProductWithVariant {
         quantity: json["quantity"],
         discountPercent: json["discount_percent"],
         stock: json["stock"],
+        stockId: json["stock_id"],
         productDescription: json["product_description"],
         images: List<String>.from(json["images"].map((x) => x)),
         unit: json["unit"],
+        variantActive: json["variant_active"],
         barcode: json["barcode"],
         draft: json["draft"],
         restockReminder: json["restock_reminder"],
-        variantActive: json["variant_active"],
+        sgst: json["SGST"],
+        cgst: json["CGST"],
+        currency: json["currency"],
       );
 
   Map<String, dynamic> toJson() => {
@@ -107,7 +115,6 @@ class ProductWithVariant {
         "category_name": categoryName,
         "category_active": categoryActive,
         "product_id": productId,
-        "currency": currency,
         "product_name": productName,
         "brand_name": brandName,
         "brand_id": brandId,
@@ -116,12 +123,16 @@ class ProductWithVariant {
         "quantity": quantity,
         "discount_percent": discountPercent,
         "stock": stock,
+        "stock_id": stockId,
         "product_description": productDescription,
         "images": List<dynamic>.from(images.map((x) => x)),
         "unit": unit,
+        "variant_active": variantActive,
         "barcode": barcode,
         "draft": draft,
         "restock_reminder": restockReminder,
-        "variant_active": variantActive
+        "SGST": sgst,
+        "CGST": cgst,
+        "currency": currency,
       };
 }
