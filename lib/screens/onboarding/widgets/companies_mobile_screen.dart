7a1b5f88f776bd4da792b1470a256108717a93eb
Sameer <gptsameer7@gmail.com>
2024-01-06 18:45:22 +0530

    Sassify_Theme_Change:Implement the UI change for companies screen

--- lib/screens/onboarding/widgets/companies_mobile_screen.dart
+++ lib/screens/onboarding/widgets/companies_mobile_screen.dart
@@ -4,13 +4,12 @@ import 'package:flutter_svg/svg.dart';
 import 'package:saasify/bloc/onboarding/onboarding_state.dart';
 import 'package:saasify/configs/app_theme.dart';
 import '../../../bloc/onboarding/onboarding_bloc.dart';
-import '../../../configs/app_spacing.dart';
 import '../../../configs/spacing.dart';
 import '../../../data/models/authentication/authentication_model.dart';
 import '../../../utils/constants/string_constants.dart';
 import '../../../widgets/primary_button.dart';
 import '../list_of_branches_screen.dart';
-import 'companies_gridview.dart';
+import 'companies_grid_view_mobile.dart';
 
 class CompaniesMobileScreen extends StatelessWidget {
   final List<Company> companyList;
@@ -21,47 +20,44 @@ class CompaniesMobileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(fit: StackFit.expand, children: [
       SvgPicture.asset("assets/gradient.svg", fit: BoxFit.fill),
-      SingleChildScrollView(
-          child: Padding(
-              padding: const EdgeInsets.all(mobileBodyPadding),
-              child: BlocBuilder<OnboardingBloc, OnboardingStates>(
-                  builder: (context, state) {
-                if (state is CompaniesLoaded) {
-                  return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                            height: MediaQuery.sizeOf(context).height * 0.12),
-                        SvgPicture.asset("assets/saasify_logo.svg",
-                            width: 50, height: 50),
-                        SizedBox(
-                            height: MediaQuery.sizeOf(context).height * 0.075),
-                        Text(StringConstants.kSelectCompany,
-                            style: Theme.of(context)
-                                .textTheme
-                                .tiniest
-                                .copyWith(fontWeight: FontWeight.w700)),
-                        const SizedBox(height: spacingLarge),
-                        CompaniesGridView(
-                            companyList: companyList,
-                            selectedCompanyIndex: state.selectedCompanyIndex),
-                        const SizedBox(height: spacingLarge),
-                        PrimaryButton(
-                            onPressed: (state.selectedCompanyIndex != -1)
-                                ? () {
-                                    Navigator.pushReplacementNamed(
-                                        context, BranchesListScreen.routeName,
-                                        arguments: companyList[
-                                            state.selectedCompanyIndex]);
-                                  }
-                                : null,
-                            buttonTitle: 'Next')
-                      ]);
-                } else {
-                  return const SizedBox.shrink();
-                }
-              })))
+      Padding(
+          padding: const EdgeInsets.all(mobileBodyPadding),
+          child: BlocBuilder<OnboardingBloc, OnboardingStates>(
+              builder: (context, state) {
+            if (state is CompaniesLoaded) {
+              return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.12),
+                    SvgPicture.asset("assets/saasify_logo.svg",
+                        width: 50, height: 50),
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.075),
+                    Text(StringConstants.kSelectCompany,
+                        style: Theme.of(context)
+                            .textTheme
+                            .tiniest
+                            .copyWith(fontWeight: FontWeight.w700)),
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.035),
+                    CompaniesGridViewMobile(
+                        companyList: companyList,
+                        selectedCompanyIndex: state.selectedCompanyIndex),
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.075),
+                    PrimaryButton(
+                        onPressed: (state.selectedCompanyIndex != -1)
+                            ? () {
+                                Navigator.pushReplacementNamed(
+                                    context, BranchesListScreen.routeName,
+                                    arguments: companyList[
+                                        state.selectedCompanyIndex]);
+                              }
+                            : null,
+                        buttonTitle: 'Next')
+                  ]);
+            } else {
+              return const SizedBox.shrink();
+            }
+          }))
     ]);
   }
 }
